c1fc7780a853d36f77f58ce09b9ae7db68320e85
Parker Ballner <pballner@gmail.com>
2017-12-10 14:36:32 -0500

    heroku commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var session = require('express-session');
 var path = require('path');
-var routes = require('./api.js');
+var routes = require('./server-side/api.js');
 
 //configure the app to use body-parser to ensure json format across all api calls
 var bodyParser = require('body-parser');
@@ -11,10 +11,10 @@ app.use(bodyParser.json());
 app.use(session({secret:'Comp426FinalProject', resave: false, saveUninitialized: true}));
 
 //Set static directories to serve to client
-app.use(express.static(path.join(__dirname + '/../client-side/')));
-app.use('/css', express.static(__dirname + '/../client-side/css'));
-app.use('/javascript', express.static(__dirname + '/../client-side/javascript'));
-app.use('/images', express.static(__dirname + '/../client-side/images'))
+app.use(express.static(path.join(__dirname + '/client-side/')));
+app.use('/css', express.static(__dirname + '/client-side/css'));
+app.use('/javascript', express.static(__dirname + '/client-side/javascript'));
+app.use('/images', express.static(__dirname + '/client-side/images'))
 
 //use the routes file to handle all apis for the app
 app.use('/', routes);
